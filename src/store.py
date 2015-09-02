ec1225051b44f85bedc739631546b35049293d58
Jeremy Gore <jmgore75@gmail.com>
2015-09-01 23:06:31 -0400

    More updates

--- src/store.py
+++ src/store.py
@@ -7,6 +7,9 @@ import os.path
 import os
 import logging
 import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('nbagg')
 
 #from sklearn.learning_curve import learning_curve
 from sklearn.externals import joblib
@@ -14,16 +17,17 @@ from sklearn.externals import joblib
 class TrainingTracker:
     def __init__(self, path, X, y):
         self.path = path
-        self.score_file = os.path.join(self.path, "scores.csv")
+        self.score_columns = ["model", "params", "train_score", "test_score", "train_time", "train_complete", "score_time", "model_bytes", "timestamp"]
+        self.score_file = os.path.join(self.path, "scores.pkl")
         self.best_file = os.path.join(self.path, "bestmodel.pkl")
         if not os.path.isdir(self.path):
             os.makedirs(self.path)
         if os.path.isfile(self.score_file):
-            self.scores = pd.read_csv(self.score_file)
+            self.scores = joblib.load(self.score_file)
             self.max_score = self.scores["test_score"].max()
         else:
-            self.scores =
-            self.max_score =
+            self.scores = pd.DataFrame(columns=self.score_columns)
+            self.max_score = 0
         self.X = X
         self.y = y
         self.num_points = len(X)
@@ -32,8 +36,66 @@ class TrainingTracker:
         self.logger = logging.getLogger(path)
 
     def standard_clfs(self):
+        yield ensemble.RandomForestClassifier(n_jobs=-1, n_estimators:16)
+        yield ensemble.RandomForestClassifier(n_jobs=-1, n_estimators:64)
+        yield ensemble.RandomForestClassifier(n_jobs=-1, n_estimators:256)
+        yield ensemble.RandomForestClassifier(n_jobs=-1, n_estimators:1024)
+        yield ensemble.ExtraTreesClassifier(n_jobs=-1, n_estimators:16)
+        yield ensemble.ExtraTreesClassifier(n_jobs=-1, n_estimators:64)
+        yield ensemble.ExtraTreesClassifier(n_jobs=-1, n_estimators:256)
+        yield ensemble.ExtraTreesClassifier(n_jobs=-1, n_estimators:1024)
+
+        yield neighbors.KNeighborsClassifier(n_neighbors=1)
+        yield neighbors.KNeighborsClassifier(n_neighbors=3)
+        yield neighbors.KNeighborsClassifier(n_neighbors=5)
+        yield neighbors.KNeighborsClassifier(n_neighbors=7)
+
+        yield svm.SVC(kernel='rbf', max_iter=2000)
+        yield svm.SVC(kernel='linear', max_iter=2000)
+        yield svm.SVC(kernel='sigmoid', C=20, max_iter=2000)
+        yield svm.SVC(kernel='poly', degree=2, max_iter=2000)
+
+        yield Classifier(
+            layers=[
+                Layer("Rectifier", name="fc0", units=100),
+                Layer("Rectifier", name="fc1", units=100),
+                Layer("Softmax")
+            ],
+            learning_rate=0.01,
+            learning_rule='momentum',
+            learning_momentum=0.9,
+            batch_size=250,
+            valid_size=0.1,
+            n_stable=20,
+            n_iter=400,
+            verbose=True)
+        yield Classifier(
+            layers=[
+                Layer("Rectifier", name="fc0", units=100),
+                Layer("Softmax")
+            ],
+            learning_rate=0.01,
+            learning_rule='momentum',
+            learning_momentum=0.9,
+            batch_size=250,
+            valid_size=0.1,
+            n_stable=20,
+            n_iter=400,
+            verbose=True)
+        yield Classifier(
+            layers=[
+                Layer("Sigmoid", name="fc0", units=100),
+                Layer("Softmax")
+            ],
+            learning_rate=0.01,
+            learning_rule='momentum',
+            learning_momentum=0.9,
+            batch_size=250,
+            valid_size=0.1,
+            n_stable=20,
+            n_iter=400,
+            verbose=True)
         yield
-        yield nolearn()
 
     def train_suite(self, clfs):
         for clf in clfs:
@@ -51,22 +113,23 @@ class TrainingTracker:
             self.logger.warning("Better model %.3f >= %.3f : %.1f s", test_score, self.max_score, train_time)
             self.max_score = test_score
             joblib.dump(clf, self.best_file)
-            performance = "Better"
         } else {
             self.logger.warning("Lesser model %.3f <  %.3f : %.1f s", test_score, self.max_score, train_time)
         }
         self.logger.info(clf)
-        self.scores.append(run)
-        self.scores.to_csv(self.score_file)
+        self.scores = self.scores.append(run, ignore_index=True)
+        joblib.dump(self.scores, self.score_file)
 
     def plot_run(self, run):
+        from IPython import display
         plt.figure()
         for model, grp in self.scores.groupby(['model']):
             plt.plot(grp['train_time'], grp['test_score'], 'o', label=model)
-        plt.plot(run["train_time"], run["test_score"], '*', color="y", s=50, label="Latest")
+        if (run):
+            plt.plot(run["train_time"], run["test_score"], '*', color="y", s=50, label="Latest")
+            plt.title('Last: %.3f; Best: %.3f' % (grp['test_score'], self.max_score))
         plt.xlabel("Train time")
         plt.ylabel("Test score")
-        plt.title('Last: %.3f; Best: %.3f' % (grp['test_score'], self.max_score))
         plt.legend(loc="best")
         plt.show()
 
